50bb348fb442b5353d50a12ceddac74ca575589a
medusa606 <greg.chance@gmail.com>
2020-08-20 13:01:13 +0100

    gc tidy up folder and tex headers

--- Results_matlab_code/Results/Determinisim_ResultsV2.m
+++ Results_matlab_code/Results/Determinisim_ResultsV2.m
@@ -58,7 +58,7 @@ set(gca, 'YScale', 'log')
 legend("Position",[0.79 0.3 0 0])
 
 %% Plot for paper (ExperimentsSummary)
-figure(3)
+figure(3); clf;
 StressArray               = ExperimentsStressSummaryData(1,2:end);
 ID1_CarsOnly              = ExperimentsStressSummaryData(2,2:end);
 ID2_CarsCollision         = ExperimentsStressSummaryData(3,2:end);
@@ -88,39 +88,62 @@ xlabel("CPU & GPU Resource Utilisation (%)")
 ylabel("max \sigma (m)")
 set(gca,'FontSize',FontSize)
 set(gca, 'YScale', 'log')
-legend("Location","East")
+% legend("Location","East")
 
 
 %% Plotting X-Y paths
-% CarsPeopleCollision_CG0 = readmatrix("TEST_CarsPeopleCollision_CG95.txt");
-% 
-% Agent1DataIndex = CarsPeopleCollision_CG0(:,2) == 1;
-% Agent1Data      = CarsPeopleCollision_CG0(Agent1DataIndex,:);
-% Agent1Type = IdentifyType(Agent1Data(1,5),Agent1Data(1,2));
-% 
-% Agent2DataIndex = CarsPeopleCollision_CG0(:,2) == 2;
-% Agent2Data      = CarsPeopleCollision_CG0(Agent2DataIndex,:);
-% Agent2Type = IdentifyType(Agent2Data(1,5),Agent2Data(1,2));
-% 
-% Agent3DataIndex = CarsPeopleCollision_CG0(:,2) == 3;
-% Agent3Data      = CarsPeopleCollision_CG0(Agent3DataIndex,:);
-% Agent3Type = IdentifyType(Agent3Data(1,5),Agent3Data(1,2));
-% 
-% for i = 1:10:max(Agent1Data(:,1))
-% RunIndexAgent1 = Agent1Data(:,1) == i;
-% RunIndexAgent2 = Agent2Data(:,1) == i;
-% RunIndexAgent3 = Agent3Data(:,1) == i;
-% 
-% figure(1)
-% plot(Agent1Data(RunIndexAgent1,8),Agent1Data(RunIndexAgent1,9),'k.','DisplayName',Agent1Type)
-% hold on
-% plot(Agent2Data(RunIndexAgent2,8),Agent2Data(RunIndexAgent2,9),'r.','DisplayName',Agent2Type)
-% hold on
-% plot(Agent3Data(RunIndexAgent3,8),Agent3Data(RunIndexAgent3,9),'y.','DisplayName',Agent3Type)
-% hold on
-% end
-%legend
+figure(1); clf
+CarsPeopleCollision_CG0 = readmatrix("TEST_CarsPeopleCollision_CG25.txt");
 
+Agent1DataIndex = CarsPeopleCollision_CG0(:,2) == 1;
+Agent1Data      = CarsPeopleCollision_CG0(Agent1DataIndex,:);
+Agent1Type = IdentifyType(Agent1Data(1,5),Agent1Data(1,2));
+
+Agent2DataIndex = CarsPeopleCollision_CG0(:,2) == 2;
+Agent2Data      = CarsPeopleCollision_CG0(Agent2DataIndex,:);
+Agent2Type = IdentifyType(Agent2Data(1,5),Agent2Data(1,2));
+
+Agent3DataIndex = CarsPeopleCollision_CG0(:,2) == 3;
+Agent3Data      = CarsPeopleCollision_CG0(Agent3DataIndex,:);
+Agent3Type = IdentifyType(Agent3Data(1,5),Agent3Data(1,2));
+
+% for i = 1:10:max(Agent1Data(:,1))
+for i = 1:max(Agent1Data(:,1))
+RunIndexAgent1 = Agent1Data(:,1) == i;
+RunIndexAgent2 = Agent2Data(:,1) == i;
+RunIndexAgent3 = Agent3Data(:,1) == i;
+
+figure(1)
+plot(Agent1Data(RunIndexAgent1,8),Agent1Data(RunIndexAgent1,9),'-k.','DisplayName',Agent1Type)
+hold on
+plot(Agent2Data(RunIndexAgent2,8),Agent2Data(RunIndexAgent2,9),'-r.','DisplayName',Agent2Type)
+hold on
+plot(Agent3Data(RunIndexAgent3,8),Agent3Data(RunIndexAgent3,9),'-b.','DisplayName',Agent3Type)
+hold on
+end
+% legend
+
+
+%% GC Edit - Show the bifurcation number at time (t)
+figure(1);clf;scatter(Agent1Data(:,8),Agent2Data(:,9)) %XY plot
+t=0.1; %(s) start of sim
+figure(2);clf;histogram(Agent1Data(Agent1Data(:,6)==t,9));
+t=10; %(s) end of sim
+figure(3);clf;histogram(Agent1Data(Agent1Data(:,6)==t,9),'NumBins',100);
+% this shows that for Car 1 there are 3/100 events outside of the normal path
+%% Agent 2
+figure;scatter(Agent2Data(:,8),Agent2Data(:,9)) %XY plot
+t=0.1; %(s) start of sim
+figure;histogram(Agent2Data(Agent2Data(:,6)==t,9));
+t=9.2; %(s) end of sim
+figure;histogram(Agent2Data(Agent2Data(:,6)==t,9));
+% this shows that 
+%%
+t=0.1; %(s) start of sim
+figure;histogram(Agent3Data(Agent3Data(:,6)==t,9));
+t=23.1; %(s) end of sim
+figure;histogram(Agent3Data(Agent3Data(:,6)==t,9));
+% this shows that 
 
 %% Functions
 function AgentType = IdentifyType(x,y)
